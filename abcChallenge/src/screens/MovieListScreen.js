006573127c6a883a43605a8567bf8fcaa23f7f00
f.mirzakhanloo <f.mirzakhanloo@dpr.co.com>
2020-10-23 22:53:56 +0330

    implement base http req methods + function to get movies on did mount

--- abcChallenge/src/screens/MovieListScreen.js
+++ abcChallenge/src/screens/MovieListScreen.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, Button } from 'react-native'
-
+import { View, Text, Button, ActivityIndicator } from 'react-native'
 import MovieDetailModal from './MovieDetailModal'
+import Http from '../tools/HttpService'
 
 const MovieListScreen = (props) => {
     const [showDetail, setDetailVisibility] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getMovies()
+    }, [])
 
     return (
         <View>
@@ -14,7 +19,24 @@ const MovieListScreen = (props) => {
                 onRequestClose={() => setDetailVisibility(false)}
                 selectedMovie='mulan'
             />
+            {loading ? <ActivityIndicator color='blue'/> : <View>
+                <Text>custom list comes here</Text>
+            </View>}
         </View>
     )
+
+
+    function getMovies() {
+        setLoading(true)
+        Http.httpGetJwt(`movies`).then(res => {
+            if (res.status == 200) {
+                console.log('___ get movies res ___', res.data.movies)
+                setLoading(false)
+            }
+        }).catch(err => {
+            console.log('___ get movies err ___', err)
+            setLoading(false)
+        })
+    }
 }
 export default MovieListScreen;
