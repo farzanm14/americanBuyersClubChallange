528d8a6f73d730acae931a3850bf00fb1f7431e2
f.mirzakhanloo <f.mirzakhanloo@dpr.co.com>
2020-10-24 16:25:17 +0330

    resolve fonticon issue + add ios simulator build

--- abcChallenge/src/screens/Home.js
+++ abcChallenge/src/screens/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Container, Text, Tab, Tabs, TabHeading, Icon } from 'native-base';
-
+import { Platform } from 'react-native'
 import MovieListScreen from './MovieListScreen'
 import SearchScreen from './SearchScreen'
 
 const Home = (props) => {
     return (
-        <Container>
+        <Container style={{ paddingTop: Platform.OS == 'ios' ? 35 : 0 ,paddingBottom: Platform.OS == 'ios' ? 15 : 0 ,}}>
             <Tabs
                 locked
                 tabBarPosition='bottom'>
-                <Tab heading={<TabHeading><Icon name="md-home-outline" /><Text>Home</Text></TabHeading>}>
+                <Tab heading={<TabHeading><Icon name="home" ios="ios-home" android="md-home"  /><Text>Home</Text></TabHeading>}>
                     <MovieListScreen />
                 </Tab>
-                <Tab heading={<TabHeading><Icon name="md-search" /><Text>Search</Text></TabHeading>}>
+                <Tab heading={<TabHeading><Icon name="search" ios="ios-search" android="md-search"  /><Text>Search</Text></TabHeading>}>
                     <SearchScreen />
                 </Tab>
             </Tabs>
